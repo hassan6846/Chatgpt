571d4aba379a332507b1a4101e0a838bd732af94
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-10-28 18:20:28 +0500

    main

---
+++ app/constants/image.tsx
@@ -0,0 +1,1 @@
+export const ChatbotAvatar:string="https://img.freepik.com/free-vector/chatbot-conversation-vectorart_78370-4107.jpg?size=338&ext=jpg&ga=GA1.1.735520172.1710288000&semt=ais"
--- app/screens/Chat/Chat.tsx
+++ app/screens/Chat/Chat.tsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { ChatbotAvatar } from '../../constants/image';
 
 const Chat = () => {
-  return (
-    <View>
-      <Text>Chat</Text>
-    </View>
-  )
-}
+  const [messages, setMessages] = useState([]);
 
-export default Chat
+  useEffect(() => {
+    // Set up initial messages if needed
+    const initialMessages = [
+      {
+        _id: 1,
+        text: 'Welcome to the chat!',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'Chatbot',
+          avatar: ChatbotAvatar,
+        },
+      },
+    ];
+    setMessages(initialMessages);
+  }, []);
+
+  const onSend = (newMessages = []) => {
+    setMessages((previousMessages) => GiftedChat.append(previousMessages, newMessages));
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+
+    </View>
+  );
+};
+
+export default Chat;
